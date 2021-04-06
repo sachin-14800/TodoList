c437f5c9b88bd59853ac097beaad69b003b43494
Sachin Gupta <sachin.14800@gmail.com>
2021-04-06 20:06:23 +0530

    express server is properly configured

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+
+
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log(`Error : ${err}`);
+        return;
+    }
+    console.log(`Server is running at port number : ${port}`);
+});
