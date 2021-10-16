81ada8991cf4bba319680e85ac340f224c5044af
sachin-14800 <sachin.14800@gmail.com>
2021-10-17 00:48:14 +0530

    cool face api added

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-//importing the database module for connection to the database
-const db=require('./config/mongoose');
-
+const cool=require('cool-ascii-faces');
 //importing the express module for the server
 const express=require('express');
 
@@ -8,8 +6,10 @@ const express=require('express');
 const app=express();
 
 //setting the port number on which the server will run
-const port=8000;
+const port=process.env.PORT || 5000;
 
+//importing the database module for connection to the database
+const db=require('./config/mongoose');
 // Setting up the view engine
 app.set('view engine','ejs');
 
@@ -18,7 +18,7 @@ app.set('views','./views');
 
 // setting the path for all the static files
 app.use(express.static('./assests'));
-
+app.get('/cool',(req,res)=>{res.send(cool())});
 // setting the path for all the routes to different locations on server
 app.use('/',require('./routes'));
 
