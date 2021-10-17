723c0fd50dd08638dc83705407aac6492f6eedf7
sachin-14800 <sachin.14800@gmail.com>
2021-10-17 15:17:53 +0530

    updated in package.json

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cool=require('cool-ascii-faces');
+
 //importing the express module for the server
 const express=require('express');
 
@@ -6,7 +6,7 @@ const express=require('express');
 const app=express();
 
 //setting the port number on which the server will run
-const port=process.env.PORT || 5000;
+const port=5000;
 
 //importing the database module for connection to the database
 const db=require('./config/mongoose');
@@ -18,7 +18,7 @@ app.set('views','./views');
 
 // setting the path for all the static files
 app.use(express.static('./assests'));
-app.get('/cool',(req,res)=>{res.send(cool())});
+
 // setting the path for all the routes to different locations on server
 app.use('/',require('./routes'));
 
